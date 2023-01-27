8c6191bc8dd2826b3e7778115d6bad7955d8fb5d
Victor Smaniotto <victorsmaniotto@hotmail.com>
2023-01-26 22:07:46 -0300

    Implementação dos seeds para testes

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('pt_BR');
+
+        User::create([
+            'nome' => 'Victor Smaniotto',
+            'email' => 'victorsmaniotto@hotmail.com',
+            'password' => bcrypt('12345678'),
+            'role' => 'admin',
+            'created_at' => now(),
+        ]);
+
+        foreach (range(1, 20) as $index) {
+            User::create([
+                'nome' => $faker->name,
+                'email' => $faker->unique()->safeEmail(),
+                'password' => bcrypt('12345678'),
+                'role' => 'editor',
+                'created_at' => now(),
+            ]);
+        }
+    }
+}
